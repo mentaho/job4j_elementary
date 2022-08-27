82fe9be59fe909c92cd70968e8536f1e7bbb1cdb
mentaho <bub.al@mail.ru>
2022-08-27 10:09:48 +0300

    5.7. Ипотека

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -5,7 +5,7 @@ public class Mortgage {
         int year = 0;
         while (amount > 0) {
             year++;
-            amount += amount * percent * .01;
+            amount += amount * percent * 0.01;
             amount -= salary;
         }
         return year;
