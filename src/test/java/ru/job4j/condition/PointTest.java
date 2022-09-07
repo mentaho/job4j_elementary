51723844ec27a007c475e98a66f0c5bb9d839e25
mentaho <bub.al@mail.ru>
2022-09-07 14:03:16 +0300

    6.9. String - это массив [#504884]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,44 +7,36 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when10to52then4dot47() {
         double expected = 4.47;
-        int x1 = 1;
-        int y1 = 0;
-        int x2 = 5;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(1, 0);
+        Point b = new Point(5, 2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when2m1tom12then4dot24() {
         double expected = 4.24;
-        int x1 = 2;
-        int y1 = -1;
-        int x2 = -1;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(2, -1);
+        Point b = new Point(-1, 2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when49to17then3dot60() {
         double expected = 3.60;
-        int x1 = 4;
-        int y1 = 9;
-        int x2 = 1;
-        int y2 = 7;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(4, 9);
+        Point b = new Point(1, 7);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
