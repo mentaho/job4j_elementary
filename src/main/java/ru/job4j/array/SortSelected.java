05febdf8d04f8e99c244c27344841997e012f65a
mentaho <bub.al@mail.ru>
2024-06-06 16:46:21 -0500

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -4,9 +4,9 @@ public class SortSelected {
 
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, i, data.length - 1);
-            int index = FindLoop.indexInRange(data, min, i, data.length - 1);
-            SwitchArray.swap(data, i, index);
+            int minValue = MinDiapason.findMin(data, i, data.length - 1);
+            int minIndex = FindLoop.indexInRange(data, minValue, i, data.length - 1);
+            SwitchArray.swap(data, i, minIndex);
         }
         return data;
     }
